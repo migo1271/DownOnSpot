0262251df9ac74476a7691018d20122be303f6b8
Mark Eijsermans <mark.eijsermans@gmail.com>
2024-08-25 02:54:49 -0700

    fix: Use cached credentials if available (#100)

--- src/spotify.rs
+++ src/spotify.rs
@@ -29,11 +29,15 @@ impl Spotify {
 		market_country_code: Option<CountryCode>,
 	) -> Result<Spotify, SpotifyError> {
 		// librespot
-		let credentials = Credentials::with_password(username, password);
+		let cache = Cache::new(Some(Path::new("credentials_cache")), None, None, None).unwrap();
+		let credentials = match cache.credentials() {
+			Some(creds) => creds,
+			None => Credentials::with_password(username, password),
+		};
 		let (session, _) = Session::connect(
 			SessionConfig::default(),
 			credentials,
-			Some(Cache::new(Some(Path::new("credentials_cache")), None, None, None).unwrap()),
+			Some(cache),
 			true,
 		)
 		.await?;
